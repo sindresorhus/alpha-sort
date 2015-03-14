a1bc075c40749774fbbc9e63c72e1a6ec75327d0
Sindre Sorhus <sindresorhus@gmail.com>
2015-03-15 00:20:51 +0800

    add fallback for broken local sorting in node 0.12 and io.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
 
-exports.asc = function (a, b) {
+// localCompare works in Node 0.10, but broken in 0.12 and io.js
+var brokenLocaleCompare = 'a'.localeCompare('å') === -1;
+
+function compare(a, b) {
+	if (brokenLocaleCompare) {
+		return a > b ? 1 : a < b ? -1 : 0;
+	}
+
 	return a === b ? 0 : a.localeCompare(b);
+}
+
+exports.asc = function (a, b) {
+	return compare(a, b);
 };
 
 exports.desc = function (a, b) {
-	return a === b ? 0 : b.localeCompare(a);
+	return compare(b, a);
 };
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var alphaSort = require('./');
 
 test(function (t) {
 	t.assert(['b', 'a', 'c'].sort(alphaSort.asc)[0] === 'a');
+	t.assert(['b', 'å', 'c'].sort(alphaSort.asc)[2] === 'å');
 	t.assert(['b', 'a', 'c'].sort(alphaSort.desc)[0] === 'c');
 	t.assert(['b', 'å', 'c'].sort(alphaSort.desc)[0] === 'å');
 	t.end();
