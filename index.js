20e23d022fc49e640a6ab25d795dbf0318028f7b
Nuno Morais <geral@nuno-morais.eu>
2016-12-14 00:32:36 +0000

    minor fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (brokenLocaleCompare) {
 	compare = function (a, b) {
 		return a > b ? 1 : a < b ? -1 : 0;
 	};
-} else if (Intl != undefined && typeof Intl.Collator === 'function') {
+} else if (global.Intl != undefined && typeof Intl.Collator === 'function') {
 	var collator = new Intl.Collator();
 	compare = function (a, b) {
 		return collator.compare(a, b);
