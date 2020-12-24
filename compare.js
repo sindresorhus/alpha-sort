ea088e0cbb632031bdc45560bc169311afc82a95
Sindre Sorhus <sindresorhus@gmail.com>
2020-12-24 14:25:10 +0700

    Update compare.js

--- compare.js
+++ compare.js
@@ -9,21 +9,21 @@ if (brokenLocaleCompare) {
 }
 
 function naturalCompare(left, right) {
-	const naturalSplitRegex = /(\d+)/; // Parentheses are important
+	const naturalSplitRegex = /(\d+)/; // Parentheses are important.
 
 	const leftChunks = left.split(naturalSplitRegex);
 	const rightChunks = right.split(naturalSplitRegex);
 
-	// If the first chunk doesn't match, the `natural` option is irrelevant
+	// If the first chunk doesn't match, the `natural` option is irrelevant.
 	if (leftChunks[0] !== rightChunks[0]) {
 		return baseCompare(left, right);
 	}
 
 	const maxValidIndex = Math.min(leftChunks.length, rightChunks.length) - 1;
 
-	// Note that `maxValidIndex` is guaranteed to be even
+	// Note that `maxValidIndex` is guaranteed to be even.
 	for (let i = 1; i < maxValidIndex; i += 2) {
-		// For odd indexes, values surely match /^\d+$/
+		// For odd indexes, values surely match `/^\d+$/`.
 		const leftNumber = Number.parseInt(leftChunks[i], 10);
 		const rightNumber = Number.parseInt(rightChunks[i], 10);
 
@@ -31,10 +31,10 @@ function naturalCompare(left, right) {
 			return leftNumber - rightNumber;
 		}
 
-		// If we're here, the numbers were equal
+		// If we're here, the numbers were equal.
 
-		// For even indexes, values surely don't match /\d/
-		// If they are not identical, the `natural` option becomes irrelevant
+		// For even indexes, values surely don't match `/\d/`.
+		// If they are not identical, the `natural` option becomes irrelevant.
 		if (leftChunks[i + 1] !== rightChunks[i + 1]) {
 			return baseCompare(
 				leftChunks.slice(i + 1).join(''),
